5f3c6bd9043bc254775ec294d76631bfd4e524d3
Richard Foo <foo.richard@gmail.com>
2018-07-08 22:38:33 -0400

    Add some components and assets.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
-  // styleUrls: ['../assets/sass/style.scss']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
